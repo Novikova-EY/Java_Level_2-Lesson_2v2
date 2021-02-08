7bc1f9e8d43571ccc49c9471a834a6374b832e44
Novikova-EY <75306313+Novikova-EY@users.noreply.github.com>
2021-02-08 10:13:00 +0300

    HomeWork_Java_Level_2-Lesson_2 version 2

---
+++ src/ru/geekbrains/java2/lesson2/Main.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.java2.lesson2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[][] array = {
+                {"11", "12", "13", "14"},
+                {"21", "22", "23", "24"},
+                {"31", "32", "33", "34"},
+                {"41", "42", "43", "44"}
+        };
+        new ArraySum().calcArray(array);
+    }
+}
+
+
